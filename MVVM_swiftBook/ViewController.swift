6a54fef3bf1b014e845175fc49aa167aabd0587a
Bair <40758299+GhostJavaWriter@users.noreply.github.com>
2022-11-10 11:45:56 +0800

    initial commit

---
+++ MVVM_swiftBook/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MVVM_swiftBook
+//
+//  Created by Bair Nadtsalov on 10.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
